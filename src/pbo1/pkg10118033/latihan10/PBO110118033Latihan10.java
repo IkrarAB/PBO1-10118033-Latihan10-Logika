878601e74886c3335cfb643e8d8d20255919350d
IkrarAB <ikraranugrah26@gmail.com>
2019-09-23 23:08:42 +0700

    Initial commit

---
+++ src/pbo1/pkg10118033/latihan10/PBO110118033Latihan10.java
@@ -0,0 +1,20 @@
+package pbo1.pkg10118033.latihan10;
+
+/*
+ *
+ * @author
+ * NAMA  : Ikrar Anugrah Bastary
+ * KELAS : PBO1
+ * NIM   : 10118033
+ *
+ */
+
+public class PBO110118033Latihan10 {
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        
+        System.out.println("Status Dewasa : " +dewasa);
+    }
+}
